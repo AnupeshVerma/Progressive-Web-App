8f8f82a95e4d297a169548fa59a52f8053017f0b
Anupesh Verma <Anupesh.verma@edugorilla.org>
2024-09-25 22:11:08 +0530

    Commit

--- app.js
+++ app.js
@@ -1,42 +1,35 @@
-// Registering ServiceWorker
+// Registering Service Worker
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('service-worker.js').then(function (registration) {
-		console.log('ServiceWorker registration successful with scope: ', registration.scope);
-	}).catch(function (err) {
-		console.log('ServiceWorker registration failed: ', err);
-	});
+    navigator.serviceWorker.register('service-worker.js').then(function (registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function (err) {
+        console.log('ServiceWorker registration failed: ', err);
+    });
 }
 
-
-let deferredPrompt
-
+let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
     deferredPrompt = e;
-    console.log("Beforeinstallprompt triggered")
-})
+    console.log("Beforeinstallprompt triggered");
+});
 
 
 const installApp = document.getElementById('installButton');
-
 installApp.addEventListener('click', async () => {
-	installSource = 'customInstallationButton';
 
-	if (deferredPrompt !== null) {
-		deferredPrompt.prompt();
-		const { outcome } = await deferredPrompt.userChoice;
-		if (outcome === 'accepted') {
-			deferredPrompt = null;
-		}
-
-		ga('send', {
-			hitType: 'event',
-			eventCategory: 'pwa-install',
-			eventAction: 'custom-installation-button-clicked',
-			eventLabel: installSource,
-			eventValue: outcome === 'accepted' ? 1 : 0
-		});
-	} else {
-		alert('Notepad is already installed.')
-	}
+    if (deferredPrompt !== null) {
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        if (outcome === 'accepted') {
+            deferredPrompt = null;
+            // Log to console or perform another action when accepted
+            console.log('User accepted the installation.');
+        } else {
+            // Log to console or perform another action when dismissed
+            console.log('User dismissed the installation.');
+        }
+    } else {
+        alert('Notepad is already installed.');
+    }
 });
